812aba8c7123b1d041f3c9c5b0f669c2e0a46216
Cleon-Jr <cleon.systi@gmail.com>
2025-01-25 00:49:19 -0400

    paycheck progress

--- resources/views/public/dashboard.blade.php
+++ resources/views/public/dashboard.blade.php
@@ -60,10 +60,9 @@
     </div>
 
     <h4 id="titlePayment" class="text-center">Folha de Pagamento</h4>
-    <hr>
 
     <div class="row" id="rowSelect">
-        <h5>Selecione um folha de pagamento <i class="bi bi-caret-right-fill"></i></h5>
+        <h5>Selecione uma folha de pagamento <i class="bi bi-caret-right-fill"></i></h5>
         <button type="button" class="btn btn-light" data-toggle="modal" data-target="#modalSelect">Clique e Selecione uma Folha de Pagamento</button>
     </div>
 
@@ -121,7 +120,104 @@
         </div>
     </div>
 
-    <div class="totals">
+    {{-- Folhas de pagamento --}}
+    <div class="accordion" id="accordionPayInfo">
+        <div class="card">
+            <div class="row">                       
+                <div class="col-md-6">
+                    <h5>Matrícula: 1234</h5>
+                </div>
+                <div class="col-md-6 text-right">
+                    <h5>Competência: 01/2024 - Sequência: 0</h5>
+                </div>
+            </div>
+            <!--                                            Cards de resumo (Button Title) -->
+            <div class="card-header" id="headerPayInfo">
+                <i id="i-left" class="bi bi-caret-down-fill"></i>
+                <i id="i-right" class="bi bi-caret-down-fill"></i>
+
+                <a href="#" data-toggle="collapse" data-target="#collapsePayInfo" aria-expanded="true" aria-controls="collapsePayInfo">
+                    <div class="totals">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="card" id="totalEarnings">
+                                    <h5>Total Poventos</h5>
+                                    <hr>
+                                    <p>R$ 1.412,00</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="card" id="discounts">
+                                    <h5>Descontos</h5>
+                                    <hr>
+                                    <p>R$ 363,50</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="card" id="netValue">
+                                    <h5>Valor Líquido</h5>
+                                    <hr>
+                                    <p>R$ 1.048,50</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>                
+            </div>
+            {{-- Collapse --}}
+            <div class="collapse show" id="collapsePayInfo" data-parent="#accordionPayInfo" aria-labelledby="headerPayInfo">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-4"><strong>SEC MUN DE DES. RURAL, PROD E ABAST</strong></div>
+                        <div class="col-sm-4"><strong>PROGRAMAS DE ASSISTENCIA SOCIAL</strong></div>
+                        <div class="col-sm-4"><strong>PROGRAMA - SAUDE DA FAMILIA - EFETIVOS</strong></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-3"><strong>Admissão:</strong> 01/11/2019</div>
+                        <div class="col-sm-2"><strong>CBO:</strong> 231205</div>
+                        <div class="col-sm-4"><strong>Cargo:</strong> AGENTE COMUNITARIO DE SAUDE</div>
+                        <div class="col-sm-2"><strong>C/HM:</strong> 200</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">                            
+                            <h5 class="text-center">Ítens de Contracheque</h5>
+                        </div>
+                    </div>                    
+                    <div class="row">
+                        <div class="col-sm-2"><strong>Código:</strong></div>
+                        <div class="col-sm-4"><strong>Descrição:</strong></div>
+                        <div class="col-sm-2"><strong>Referência:</strong></div>
+                        <div class="col-sm-2"><strong>Vencimentos:</strong></div>
+                        <div class="col-sm-2"><strong>Descontos:</strong></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-2">1</div>
+                        <div class="col-sm-4">SALÁRIO BASE</div>
+                        <div class="col-sm-2">30,00</div>
+                        <div class="col-sm-2">1.458,54</div>
+                        <div class="col-sm-2"></div>                        
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-2">997</div>
+                        <div class="col-sm-4">INSS</div>
+                        <div class="col-sm-2">12,00</div>
+                        <div class="col-sm-2"></div>
+                        <div class="col-sm-2">178,32</div>                        
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            Imprimir Contracheque
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- <div class="totals">
         <div class="row">
             <div class="col-md-4">
                 <div class="card" id="totalEarnings">
@@ -145,7 +241,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
 
 </section>
